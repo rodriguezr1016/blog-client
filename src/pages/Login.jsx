d14a64e19e3af582de1455bf585940757298fe49
rodriguezr1016 <134916213+rodriguezr1016@users.noreply.github.com>
2024-03-21 09:29:56 -0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login (){
     
     async function login(ev) {
         ev.preventDefault();
-        const response = await fetch('http://localhost:4000/login',{
+        const response = await fetch('https://yvettes-mern-blog-b1h4ysmae-rene-rodriguezs-projects.vercel.app/login',{
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: {'Content-Type': 'application/json'},
